dc04a0c75a55adfcdebdb03e49bdc4911e6cc3a1
Ashish Nath <otakuash11apr@gmail.com>
2023-08-29 22:40:46 +0530

    Add more functionalities like start/re-start btn

--- script.js
+++ script.js
@@ -7,15 +7,19 @@ const gameBoard = ( () => {
 )();
 
 
+//------------------------------------------------------------------------//----------------------------------------------------------------------------//
 //factory to create players
 const Players = (name, symbol) => {
     return{name, symbol};
 }
 
-const player1 = Players('ash', 'x');
-const player2 = Players('himu', 'o');
+// const player1 = Players('ash', 'x');
+// const player2 = Players('himu', 'o');
+let player1;
+let player2;
 
 
+//------------------------------------------------------------------------//----------------------------------------------------------------------------//
 //module to control the flow of the game
 const flow = ( () => {
 
@@ -43,14 +47,17 @@ const flow = ( () => {
                 console.log(cell);
                 player = player1;
                 flag = 1;
+                pTurn.children[1].innerText = player2.name;
+                e.target.style.color = 'red';
             }
             else{
                 cell = e.target.dataset.value;
                 console.log(cell);
                 player = player2;
                 flag = 0;
+                pTurn.children[1].innerText = player1.name;
+                e.target.style.color = 'green';
             }
-
             e.target.innerText = player.symbol;
             mark(player, cell);
         }  
@@ -90,7 +97,8 @@ const flow = ( () => {
         }
 
         if(win){
-            alert(`The winner is: ${win}`);
+            winMsg.children[0].innerText = 'We Have a WINNER!';
+            winMsg.children[1].innerText = `The winner is ${win}`;
             return true;
         }
 
@@ -100,12 +108,13 @@ const flow = ( () => {
         }
 
         if(total == 9){
-            alert("It's a draw");
+            winMsg.children[0].innerText = 'You played neck to neck!';
+            winMsg.children[1].innerText = "It's a draw";
             return true;
         }
     };
 
-//------------------------------------------------------------------------//----------------------------------------------------------------------------//
+    //------------------------------------------------------------------------//----------------------------------------------------------------------------//
         //DOM MANIPULATION
         const divGameBoard = document.querySelector('.gameBoard');
         let divCell;
@@ -120,9 +129,37 @@ const flow = ( () => {
         }
     
         let dValue = document.querySelectorAll('.cell');
-        for(let i = 0; i < 9; i++){
-            dValue[i].addEventListener('click', chance);
-        }      
+        // for(let i = 0; i < 9; i++){
+        //     dValue[i].addEventListener('click', chance);
+        // }      
+        let winMsg = document.querySelector('.winner');
+        let pTurn = document.querySelector('.pTurn');
+        let pName = document.querySelector('.pName');
+        let startBtn = document.querySelector('.startBtn');
+        startBtn.addEventListener('click', () => { 
+            let p1 = prompt("Player 1, enter your name");
+            let p2 = prompt("player 2, enter your name");
+
+            player1 = Players(p1.toUpperCase(), 'x');
+            player2 = Players(p2.toUpperCase(), 'o');
+
+            pName.children[0].children[0].innerText = 'Player 1:';
+            pName.children[0].children[1].innerText = player1.name;
+            pName.children[1].children[0].innerText = 'Player 2:';
+            pName.children[1].children[1].innerText = player2.name;  
+            
+            pTurn.children[0].innerText = 'Current Player is:'; 
+            pTurn.children[1].innerText = player1.name;
+
+            for(let i = 0; i < 9; i++){
+                dValue[i].addEventListener('click', chance);
+            }
+        })
+
+        let reStart = document.querySelector('.reStart');
+        reStart.addEventListener('click', () => {
+            window.location.reload();
+        })
     
 })();
 
